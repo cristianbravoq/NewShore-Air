5dbadc3e5770919b3a7439284a300ae17da11e48
Cristian Bravo <75435035+cristianbravoq@users.noreply.github.com>
2022-04-25 00:24:13 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path:'', redirectTo:'/home', pathMatch: 'full'},
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path:'**', redirectTo:'/home', pathMatch: 'full'},
+  { path:'**', redirectTo:'/home', pathMatch: 'full'}
 ];
 
 @NgModule({
