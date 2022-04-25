c3f25e991e4ff309a1661a70918e1c3686e1c6e8
Cristian Bravo <75435035+cristianbravoq@users.noreply.github.com>
2022-04-25 00:17:32 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path:'', redirectTo:'/home', pathMatch: 'full'},
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path:'**', redirectTo:'/home', pathMatch: 'full'},
 ];
 
